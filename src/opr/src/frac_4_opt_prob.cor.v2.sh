bae74aa7a320c7b8f02f85f752bb0951810a2138
PeitaoPeng <peitaopeng@icloud.com>
2025-05-29 03:00:01 -0400

    Daily commit for Thu May 29 03:00:01 EDT 2025

--- src/opr/src/frac_4_opt_prob.cor.v2.sh
+++ src/opr/src/frac_4_opt_prob.cor.v2.sh
@@ -49,7 +49,8 @@ cd $tmp
 for curyr in 2021 2022 2023 2024; do
 #for curyr in 2024; do
 #curmt=`date --date='today' '+%m'`  # mo of making fcst
-for curmo in 01 02 03 04 05 06 07 08 09 10 11 12; do
+#for curmo in 01 02 03 04 05 06 07 08 09 10 11 12; do
+for curmo in 01 04 05 06 07 08 09 10 11 12; do
 #for curmo in 11; do
 #
 if [ $curmo = 01 ]; then cmon=1; icmon=12; icmonc=dec; tgtmon=feb; tgtss=fma; fi #tgtmon:1st mon of the lead-1 season
