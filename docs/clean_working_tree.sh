09b943dec44f7860fc5296e6762775f9c660d9c2
PeitaoPeng <peitaopeng@icloud.com>
2026-03-05 16:33:31 -0500

    Update forecasts for YEAR=2025

---
+++ docs/clean_working_tree.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo 
+
+# ============================================================
+# Clean the working tree before run pipline file
+# Usage:
+#     ./clean_working_tree.sh
+# ============================================================
+
+cd ~/ClimateInform
+git add -A
+git commit -m "Add 2026 forecast HTML pages"
+git pull --rebase
+
+echo "============================================================"
+echo " clean working tree completed
+echo "============================================================"
+
