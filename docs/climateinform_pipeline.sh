495cece0ad087b4512e73f60a2273586eff35c00
PeitaoPeng <peitaopeng@icloud.com>
2026-03-05 16:27:56 -0500

    Add 2026 forecast HTML pages

--- docs/climateinform_pipeline.sh
+++ docs/climateinform_pipeline.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 #curyr=`date --date='today' '+%Y'`
 #curmo=`date --date='today' '+%m'`  # current month
 
-curyr=2026
-#for curmo in 01 02 03 04 05 06 07 08 09 10 11 12; do
-for curmo in 01 02; do
+curyr=2025
+for curmo in 01 02 03 04 05 06 07 08 09 10 11 12; do
+#for curmo in 01 02; do
 #
 if [ $curmo = 01 ]; then cmon=1; fi
 if [ $curmo = 02 ]; then cmon=2; fi
